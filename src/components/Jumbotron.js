63318541716ca426713343973dbd70b065c939e9
Andrew Waterhouse <gudolph@gmail.com>
2019-04-10 21:06:29 -0600

    trying to make it all master level

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron() {
+    return <div className="jumbotron">
+        <h1>ExpLore</h1>
+        <p>Character:</p>
+        <p>Score:</p>
+        <p>
+            <button className="btn btn-primary btn-lg">Logout</button>
+        </p>
+    </div>
+}
+
+export default Jumbotron;
