4b7ad7c42d6b12d464c4e9a495795b6e21d34dba
Erin Hancock <welljer@gmail.com>
2019-04-11 20:13:10 -0600

    routeControllerSomeHerokuSetUp

--- models/index.js
+++ models/index.js
@@ -1,3 +1,28 @@
-module.exports = {
-    User: require('./user.js')
-  };
+const mongoose = require('mongoose');
+const URI = require('../view/config');
+
+
+//Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || URI);
+
+// connects our back end code with the db
+mongoose.connect(
+  URI,
+  { useNewUrlParser: true }
+);
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', function () {  
+  console.log('Established mongoose default connection');
+}); 
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {  
+  console.log('Mongoose default connection error: ' + err);
+}); 
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {  
+  console.log('Mongoose default connection disconnected'); 
+});
