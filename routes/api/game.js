5ce70c10ad6781dad07713067fe977539b55cde5
Jeremy Cantwell <welljer@gmail.com>
2019-04-15 20:08:45 -0600

    corrected lat/lng set up

---
+++ routes/api/game.js
@@ -0,0 +1,8 @@
+// const express = require("express");
+// const router = express.Router();
+// const keys = require("../../config/keys");
+
+// // load game model
+// const Game = require("../../models/game.js");
+
+// // post 
