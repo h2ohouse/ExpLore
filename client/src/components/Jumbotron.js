d93b746d8cd1cfdc4f10986afa0e48e5e539c202
Andrew Waterhouse <gudolph@gmail.com>
2019-04-27 23:41:59 -0600

    Blah

--- client/src/components/Jumbotron.js
+++ client/src/components/Jumbotron.js
@@ -4,9 +4,8 @@ import Sword from './Images/sword.png'
 
 function Jumbotron() {
     return <div className="jumbotron">
-    <img className="logo" src={Sword} width='50px' height="50px" style={{borderStyle: 'solid', borderColor: '#8E8269'}}/>
         <h1 className="jumbo-text">
-        
+        <img className="logo" src={Sword} width="10%" height="auto" style={{borderStyle: 'solid', borderColor: '#8E8269'}}/>    
          xpLore
         </h1>
     </div>
